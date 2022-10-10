e547ce8ed11ed5416c795aad3e62a3991741d194
aito <BoxPistols@users.noreply.github.com>
2022-10-10 21:24:15 +0900

    refact get param

--- app.js
+++ app.js
@@ -1,25 +1,39 @@
+/* change toggle card view */
 document.querySelectorAll("button").forEach((e) => {
   e.addEventListener("click", () => {
-    // const cardWidth = document.querySelector(".card")
-    const elements = document.querySelectorAll(".card")
+    const cardWidth = document.querySelector(".card")
     for (let index = 0; index < elements.length; index += 1) {
       const element = elements[index]
       element.style.width = e.dataset.size
-      console.log(element.textContent)
+      // console.log(element.textContent)
     }
   })
 })
-
+/* view card width */
 addEventListener("click", () => {
-  const cardWidth = document.querySelector(".card")
+  const card = document.querySelector(".card")
   const view = document.querySelector(".view")
-  const style = window.getComputedStyle(cardWidth)
-  view.innerHTML = style.width
+  const styleCard = window.getComputedStyle(card)
+  let paddingCard = parseInt(styleCard.paddingLeft) + parseInt(styleCard.paddingRight)
+  view.innerHTML = styleCard.width + " & padding-total: " + paddingCard + "px"
   // console.log(style.width)
 })
 
+/* get style on with loop */
 const elements = document.querySelectorAll(".card")
-for (let index = 0; index < elements.length; index += 1) {
-  const element = elements[index]
-  console.log(element.textContent)
-}
+
+// for (let index = 0; index < elements.length; index += 1) {
+//   const el = elements[index]
+// }
+// console.log(el.style)
+// console.log(el.textContent)
+
+// elements.forEach((el) => console.log(el.style))
+
+/* add class test */
+elements.forEach((el, index) => {
+  if (index === 1) {
+    el.classList.add('classic');
+  }
+
+});
