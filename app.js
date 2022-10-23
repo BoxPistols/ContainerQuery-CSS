9d29885312e8303fd9fb0488f6bf04e61e4c688a
aito <BoxPistols@users.noreply.github.com>
2022-10-24 05:50:25 +0900

    js update

--- app.js
+++ app.js
@@ -1,53 +1,50 @@
-/**
- * Card
- */
-const elements = document.querySelectorAll(".card")
+{
+  /* --- resize width --- */
+  const widthCalculation = () => {
+    const wrapElement = document.querySelector(".wrap")
+    const styleW = window.getComputedStyle(wrapElement)
+    const windowView = document.querySelector(".windowView")
+    windowView.textContent = styleW.width
+  }
 
-/* change toggle card view */
-document.querySelectorAll("button").forEach((e) => {
-  e.addEventListener("click", () => {
-    for (let index = 0; index < elements.length; index += 1) {
-      const element = elements[index]
-      element.style.width = e.dataset.size
-      // console.log(element.textContent)
-    }
+  widthCalculation()
+
+  /* --- card --- */
+  const elements = document.querySelectorAll(".card")
+  // @note result view card width
+
+  const widthCards = () => {
     const card = document.querySelector(".card")
     const view = document.querySelector(".view")
     const styleCard = window.getComputedStyle(card)
     let paddingCard =
       parseInt(styleCard.paddingLeft) + parseInt(styleCard.paddingRight)
     view.innerHTML = styleCard.width + " & padding-total: " + paddingCard + "px"
+  }
+  widthCards()
+
+  window.onresize = function () {
+    widthCalculation()
+    widthCards()
+  }
+
+  /* change toggle card view */
+  document.querySelectorAll("button").forEach((e) => {
+    e.addEventListener("click", () => {
+      // @note click button & change card size
+      for (let index = 0; index < elements.length; index += 1) {
+        const element = elements[index]
+        element.style.width = e.dataset.size
+        // console.log(element.textContent)
+      }
+      widthCards()
+    })
   })
-})
 
-// Resize Width
-function widthCalculation() {
-  const wrapElement = document.querySelector(".wrap")
-  const styleW = window.getComputedStyle(wrapElement)
-  const windowView = document.querySelector(".windowView")
-  windowView.textContent = styleW.width
+  /* add class test */
+  elements.forEach((el, index) => {
+    // if (index === 1) {
+    el.classList.add("card-" + (index + 1))
+    // }
+  })
 }
-
-widthCalculation()
-
-window.onresize = function () {
-  widthCalculation()
-}
-/* view card width
-
-addEventListener("click", () => {
-  const card = document.querySelector(".card")
-  const view = document.querySelector(".view")
-  const styleCard = window.getComputedStyle(card)
-  let paddingCard =
-  parseInt(styleCard.paddingLeft) + parseInt(styleCard.paddingRight)
-  view.innerHTML = styleCard.width + " & padding-total: " + paddingCard + "px"
-})
-*/
-
-/* add class test */
-elements.forEach((el, index) => {
-  // if (index === 1) {
-  el.classList.add("card-" + (index + 1))
-  // }
-})
