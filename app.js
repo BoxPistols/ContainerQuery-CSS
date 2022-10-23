529b3918959e9018b83578be6029b32179f8aa6d
aito <BoxPistols@users.noreply.github.com>
2022-10-24 05:09:53 +0900

    move size View

--- app.js
+++ app.js
@@ -1,39 +1,53 @@
+/**
+ * Card
+ */
+const elements = document.querySelectorAll(".card")
+
 /* change toggle card view */
 document.querySelectorAll("button").forEach((e) => {
   e.addEventListener("click", () => {
-    const cardWidth = document.querySelector(".card")
     for (let index = 0; index < elements.length; index += 1) {
       const element = elements[index]
       element.style.width = e.dataset.size
       // console.log(element.textContent)
     }
+    const card = document.querySelector(".card")
+    const view = document.querySelector(".view")
+    const styleCard = window.getComputedStyle(card)
+    let paddingCard =
+      parseInt(styleCard.paddingLeft) + parseInt(styleCard.paddingRight)
+    view.innerHTML = styleCard.width + " & padding-total: " + paddingCard + "px"
   })
 })
-/* view card width */
+
+// Resize Width
+function widthCalculation() {
+  const wrapElement = document.querySelector(".wrap")
+  const styleW = window.getComputedStyle(wrapElement)
+  const windowView = document.querySelector(".windowView")
+  windowView.textContent = styleW.width
+}
+
+widthCalculation()
+
+window.onresize = function () {
+  widthCalculation()
+}
+/* view card width
+
 addEventListener("click", () => {
   const card = document.querySelector(".card")
   const view = document.querySelector(".view")
   const styleCard = window.getComputedStyle(card)
-  let paddingCard = parseInt(styleCard.paddingLeft) + parseInt(styleCard.paddingRight)
+  let paddingCard =
+  parseInt(styleCard.paddingLeft) + parseInt(styleCard.paddingRight)
   view.innerHTML = styleCard.width + " & padding-total: " + paddingCard + "px"
-  // console.log(style.width)
 })
-
-/* get style on with loop */
-const elements = document.querySelectorAll(".card")
-
-// for (let index = 0; index < elements.length; index += 1) {
-//   const el = elements[index]
-// }
-// console.log(el.style)
-// console.log(el.textContent)
-
-// elements.forEach((el) => console.log(el.style))
+*/
 
 /* add class test */
 elements.forEach((el, index) => {
-  if (index === 1) {
-    el.classList.add('classic');
-  }
-
-});
+  // if (index === 1) {
+  el.classList.add("card-" + (index + 1))
+  // }
+})
